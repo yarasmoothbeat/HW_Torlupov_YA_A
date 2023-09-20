1673dfc8ce2ca82cad26fc7dd19ed9469a2bb991
master <yarasmoothbeat@gmail.com>
2023-09-20 11:05:05 +0500

    Задача 8

---
+++ Задача8/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = 8;
+int count = 1;
+
+while   ( count <= n )
+{
+    if ( count % 2 > 0 )
+        {
+            count++;
+        }
+    if ( count % 2 == 0);
+        {
+            Console.WriteLine ( count );
+            count++;
+        }
+}
